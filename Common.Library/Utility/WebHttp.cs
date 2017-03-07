2673ab0bf2b1af62cd2c9ad9bc98d251d59938c1
小志 <1103397760@qq.com>
2017-03-07 10:01:39 +0800

    Optimization for WebHttp code

--- Common.Library/Utility/WebHttp.cs
+++ Common.Library/Utility/WebHttp.cs
@@ -43,20 +43,6 @@ namespace Common.Utility
             return (T)DeserializeObject<T>(result);
         }
 
-        /// <summary>发送请求</summary>
-        /// <param name="url">地址</param>
-        /// <param name="param">参数</param>
-        /// <param name="type">类型</param>
-        /// <param name="contenttype">请求相应格式application/x-www-form-urlencoded,默认:application/json</param>
-        /// <returns>本次请求的响应结果</returns>
-        public static string SendRequest(string url, string param, string type, string contenttype = "application/json")
-        {
-            var responseMessage = SendHttpRequeset(url, param, type, contenttype);
-            if (responseMessage.IsSuccessStatusCode)
-                return responseMessage.Content.ReadAsStringAsync().Result;
-            return string.Empty;
-        }
-
         #endregion
 
         #region SendPut
@@ -77,11 +63,10 @@ namespace Common.Utility
         #region SendDelete
 
         /// <summary>发送Put请求</summary>
-        /// <typeparam name="T">参数类型</typeparam>
         /// <param name="url">地址</param>
         /// <param name="param">参数</param>
         /// <returns>本次请求的响应结果</returns>
-        public static HttpResponseMessage SendDelete<T>(string url, T param)
+        public static HttpResponseMessage SendDelete(string url, string param)
         {
             return SendHttpRequeset(url, param, "delete");
         }
@@ -89,35 +74,41 @@ namespace Common.Utility
         #endregion
 
         #region SendHttpRequeset
-
-        /// <summary>发送Http请求</summary>
-        /// <typeparam name="T">参数数据类型</typeparam>
-        /// <param name="url">请求地址</param>
-        /// <param name="param">请求参数</param>
-        /// <param name="requestType">请求方式</param>
-        /// <returns></returns>
-        private static HttpResponseMessage SendHttpRequeset<T>(string url, T param, string requestType)
+        
+        /// <summary>发送请求</summary>
+        /// <param name="url">地址</param>
+        /// <param name="param">参数</param>
+        /// <param name="requesttype">类型</param>
+        /// <param name="charset">字符集格式</param>
+        /// <param name="contenttype">请求相应格式application/x-www-form-urlencoded,或 application/json</param>
+        /// <returns>本次请求的响应结果</returns>
+        public static string SendRequest(string url, string param, string requesttype, string charset = "utf-8", string contenttype = "application/x-www-form-urlencoded")
         {
-            return SendHttpRequeset(url, param, requestType, "application/json");
+            var responseMessage = SendHttpRequeset(url, param, requesttype, charset, 0, contenttype);
+            if (responseMessage.IsSuccessStatusCode)
+                return responseMessage.Content.ReadAsStringAsync().Result;
+            return string.Empty;
         }
 
         /// <summary>发送Http请求</summary>
-        /// <typeparam name="T">参数数据类型</typeparam>
         /// <param name="url">请求地址</param>
         /// <param name="param">请求参数</param>
-        /// <param name="requestType">请求方式</param>
+        /// <param name="requesttype">请求方式</param>
+        /// <param name="charset">字符集格式</param>
+        /// <param name="timeout">请求超时时间</param>
         /// <param name="contenttype">媒体类型</param>
         /// <returns></returns>
-        private static HttpResponseMessage SendHttpRequeset<T>(string url, T param, string requestType, string contenttype)
+        private static HttpResponseMessage SendHttpRequeset(string url, string param, string requesttype, string charset = "utf-8", int timeout = 0, string contenttype = "application/x-www-form-urlencoded")
         {
             //设置HttpClientHandler的AutomaticDecompression
             var handler = new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip };
             using (var client = new HttpClient(handler))
             {
+                if (timeout > 0) { client.Timeout = TimeSpan.FromMilliseconds(timeout); }
                 var context = new StringContent(param.ToString());
-                context.Headers.ContentType = new MediaTypeHeaderValue(contenttype) { CharSet = "utf-8" };
+                context.Headers.ContentType = new MediaTypeHeaderValue(contenttype) { CharSet = charset };
 
-                switch (requestType)
+                switch (requesttype)
                 {
                     case "get":
                         return client.GetAsync(url).Result;
@@ -143,12 +134,9 @@ namespace Common.Utility
         /// <returns></returns>
         private static object DeserializeObject<T>(HttpResponseMessage httpResponseMessage)
         {
-            if (httpResponseMessage == null) return null;
-            var resultJson = "";
-            if (httpResponseMessage.IsSuccessStatusCode)
-                resultJson = httpResponseMessage.Content.ReadAsStringAsync().Result;
-            if (string.IsNullOrEmpty(resultJson)) return null;
-            return JsonConvert.DeserializeObject<T>(resultJson);
+            if (httpResponseMessage == null) return default(T);
+            var resultJson = httpResponseMessage.IsSuccessStatusCode ? httpResponseMessage.Content.ReadAsStringAsync().Result : string.Empty;
+            return string.IsNullOrEmpty(resultJson) ? default(T) : JsonConvert.DeserializeObject<T>(resultJson);
         }
 
         #endregion
@@ -157,18 +145,18 @@ namespace Common.Utility
 
         /// <summary>发送GET请求，接收返回数据</summary>
         /// <param name="url"></param>
-        /// <param name="endoding"></param>
+        /// <param name="charset"></param>
         /// <returns></returns>
-        public static string HttpWebRequestGet(string url, string endoding = "ASCII")
+        public static string HttpWebRequestGet(string url, string charset = "ASCII")
         {
             try
             {
                 string result;
-                // System.Net.ServicePointManager.DefaultConnectionLimit=100;
+                // System.Net.ServicePointManager.DefaultConnectionLimit=100; // 设置最大请求连接数
                 var request = WebRequest.Create(url);
                 using (var response = request.GetResponse())
                 {
-                    var resReader = new StreamReader(response.GetResponseStream() ?? Stream.Null, Encoding.GetEncoding(endoding), false);
+                    var resReader = new StreamReader(response.GetResponseStream() ?? Stream.Null, Encoding.GetEncoding(charset), false);
                     result = resReader.ReadToEnd();
                     resReader.Close();
                     resReader.Dispose();
@@ -183,36 +171,29 @@ namespace Common.Utility
         /// <typeparam name="T">返回的数据对象</typeparam>
         /// <param name="url">请求地址</param>
         /// <param name="param">请求提交参数对象</param>
+        /// <param name="setCode">设置所发送的请求提交参数编码格式</param>
         /// <returns>返回 T 数据对象</returns>
         public static T HttpWebRequestPost<T>(string url, string param)
         {
-            return HttpWebRequestPost<T>(url, param, 0, "UTF-8", "application/json");
+            string result = HttpWebRequestPost(url, param, 0, "utf-8", "application/json");
+            return string.IsNullOrEmpty(result) ? default(T) : JsonConvert.DeserializeObject<T>(result);
         }
-        /// <summary>发送POST请求，接受返回数据</summary>
-        /// <typeparam name="T">返回的数据对象</typeparam>
-        /// <param name="url">请求地址</param>
-        /// <param name="param">请求提交参数对象</param>
-        /// <param name="setCode">设置所发送的请求提交参数编码格式</param>
-        /// <returns>返回 T 数据对象</returns>
-        public static T HttpWebRequestPost<T>(string url, string param, string setCode)
-        {
-            return HttpWebRequestPost<T>(url, param, 0, setCode, "application/json");
-        }
+
         /// <summary>发送POST请求，接受返回数据</summary>
         /// <typeparam name="T">返回的数据对象</typeparam>
         /// <param name="url">请求地址</param>
         /// <param name="param">请求提交参数对象</param>
         /// <param name="timeout">设置所发送的请求超时时间</param>
-        /// <param name="setCode">设置所发送的请求提交参数编码格式</param>
+        /// <param name="charset">设置所发送的请求提交参数编码格式</param>
         /// <param name="contentType">设置所发送的请求数据的内容类型,默认 application/x-www-form-urlencoded 格式</param>
         /// <returns>返回 T 数据对象</returns>
-        public static T HttpWebRequestPost<T>(string url, string param, int timeout, string setCode, string contentType = "application/x-www-form-urlencoded")
+        public static string HttpWebRequestPost(string url, string param, int timeout, string charset = "ASCII", string contentType = "application/x-www-form-urlencoded")
         {
             try
             {
                 string result;
-                // System.Net.ServicePointManager.DefaultConnectionLimit=100;
-                Encoding encode = Encoding.GetEncoding(setCode);
+                // System.Net.ServicePointManager.DefaultConnectionLimit=100; // 设置最大请求连接数
+                Encoding encode = Encoding.GetEncoding(charset);
                 var by = encode.GetBytes(param);
                 var request = WebRequest.Create(url);
                 if (timeout > 0) { request.Timeout = timeout; }
@@ -234,9 +215,9 @@ namespace Common.Utility
                     response.Close();
                 }
                 request.Abort();
-                return JsonConvert.DeserializeObject<T>(result);
+                return result;
             }
-            catch (Exception ex) { Log.Error(ex); return default(T); }
+            catch (Exception ex) { Log.Error(ex); return string.Empty; }
         }
 
         #endregion
