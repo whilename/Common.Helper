bf7b3cf1d520adaff642b27473c3fe6bd2041ad8
小志 <1103397760@qq.com>
2017-09-18 11:11:25 +0800

    Update EnumExtension.cs

--- Common.Library/Extension/EnumExtension.cs
+++ Common.Library/Extension/EnumExtension.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Common.Extension
+namespace System.Extension
 {
     /// <summary></summary>
     public static class EnumExtensions
