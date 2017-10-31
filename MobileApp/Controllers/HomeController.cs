6214fa0a15bdd4fe49be96ed46c74926fe2d898c
Qunzhi Ouyang <qunzhi.ouyang@thermofisher.com>
2017-10-31 21:21:06 +0800

    Modify mobileapp logic

--- MobileApp/Controllers/HomeController.cs
+++ MobileApp/Controllers/HomeController.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WeChat;
+using WeChat.Models;
 
 namespace MobileApp.Controllers
 {
     /// <summary></summary>
     public class HomeController : Controller
     {
-        /// <summary></summary>
-        public ActionResult Index()
+        /// <summary>Authorize redirect url.</summary>
+        /// <param name="state">Paramenters</param>
+        /// <param name="silent">Is Silent Authorize,0 silent,1 use authorize</param>
+        /// <returns>Redirect Authorize Page</returns>
+        public RedirectResult Auth(string state = "", int silent = 1)
+        {
+            string back_url = Request.Url.AbsoluteUri + "/Index";
+            return Redirect(CorpCommon.Instance.Oauth2Authorize(back_url, state, silent == 0));
+        }
+
+        /// <summary>Index page</summary>
+        /// <param name="code">Authorization code</param>
+        /// <param name="state">Paramenters</param>
+        /// <returns></returns>
+        public ActionResult Index(string code, string state = "")
         {
             //ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            MemberTicket ticket = CorpCommon.Instance.GetMemberTicket(code);
+            WXMemberInfo member = CorpCommon.Instance.GetMemberInfo(ticket.user_ticket);
 
-            return View();
+            return View(member);
         }
 
         /// <summary></summary>
